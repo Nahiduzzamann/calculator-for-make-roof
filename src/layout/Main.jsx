8ebd382094037a9cd61857a40baa18c21b69c784
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-07-14 17:49:14 +0100

    added nav and firebase

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../pages/Shared/Header/Navbar";
 
 
 const Main = () => {
     return (
         <div>
-            {/* <Navbar></Navbar> */}
+            <Navbar></Navbar>
             <Outlet></Outlet>
             {/* <Footer></Footer> */}
         </div>
